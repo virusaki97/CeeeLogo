92a0a48f11d764ee2036c0b6e1d7065f27756de4
virusaki97 <virusaki97@github.com>
2021-04-20 08:54:00 +0300

    first commit

---
+++ logo_anim.js
@@ -0,0 +1,16 @@
+document.getElementById( 'slidexs' ).addEventListener( 'click', function() {
+
+			this.style.opacity == '0' || this.style.opacity == ''
+			? this.style.opacity = '1' 
+			: this.style.opacity = '0';
+
+			}, false );
+			
+			
+document.getElementById( 'slide' ).addEventListener( 'click', function() {
+
+			this.style.opacity == '0' || this.style.opacity == ''
+			? this.style.opacity = '1' 
+			: this.style.opacity = '0';
+
+			}, false );
